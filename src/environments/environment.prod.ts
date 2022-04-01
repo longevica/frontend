960b20ea4ff2756f3a3a95343f8dec7304655eed
Constantine <kon.rafikov@yandex.ru>
2022-04-01 00:21:54 -0300

    Set up environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   name: 'prod',
   production: true,
-  apiUrl: 'https://test.open-drugs.org/',
+  apiUrl: 'https://d4kppyp4im.us-east-2.awsapprunner.com/',
   apiMocks: {
     experimentsList: '../assets/mocks/experiments-list-mock.json',
     speciesList: '../assets/mocks/species-list-mock.json',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   name: 'default',
   production: false,
-  apiUrl: 'https://test.open-drugs.org/',
+  apiUrl: 'https://d4kppyp4im.us-east-2.awsapprunner.com/',
   apiMocks: {
     experimentsList: '../assets/mocks/experiments-list-mock.json',
     speciesList: '../assets/mocks/species-list-mock.json',
